8a4042d0e5f3815e26aa0644eac9034aea5faa8a
chunyangl <chunyangl@jumei.com>
2019-11-12 18:18:36 +0800

    f

--- src/views/cheyijia/point/add.vue
+++ src/views/cheyijia/point/add.vue
@@ -305,14 +305,14 @@ export default {
     },
     // 百度经纬度转高德经纬度
     bd_decrypt(bd_lng, bd_lat) {
-      var X_PI = Math.PI * 3000.0 / 180.0;
-      var x = bd_lng - 0.0065;
-      var y = bd_lat - 0.006;
-      var z = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * X_PI);
-      var theta = Math.atan2(y, x) - 0.000003 * Math.cos(x * X_PI);
-      var gg_lng = z * Math.cos(theta);
-      var gg_lat = z * Math.sin(theta);
-      return {lng: gg_lng, lat: gg_lat}
+      var X_PI = Math.PI * 3000.0 / 180.0
+      var x = bd_lng - 0.0065
+      var y = bd_lat - 0.006
+      var z = Math.sqrt(x * x + y * y) - 0.00002 * Math.sin(y * X_PI)
+      var theta = Math.atan2(y, x) - 0.000003 * Math.cos(x * X_PI)
+      var gg_lng = z * Math.cos(theta)
+      var gg_lat = z * Math.sin(theta)
+      return { lng: gg_lng, lat: gg_lat }
     },
     getAreaMsg() {
       return this.checkArea !== '' ? this.areaData[0][this.checkArea] : ''
